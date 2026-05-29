4fe6b5d257942ddf29fef8eebca51e6be4122baf
Ryan Dewey <145182587+RyanDewey@users.noreply.github.com>
2026-05-29 21:25:11 +0000

    Cleaned up code, moved definitions inside class

--- main.cpp
+++ main.cpp
@@ -20,19 +20,43 @@ int add(int a, int b) {
     return a + b;
 }
 
+struct MultiplyBy {
+    int factor;
+
+    MultiplyBy(int f) : factor(f) {}
+
+    int operator()(int n) const {
+        return n * factor;
+    }
+};
+
+class Num {
+    public:
+        int makeInt(double x) { return int(x); }
+};
+
 int main() {
 
     ThreadPool pool;
+
+    MultiplyBy triple(3);
+    Num myNum;
     
     pool.submit(sayHi);
     auto fut1 = pool.submit(retThree);
     auto fut2 = pool.submit(retThreePointThree);
     auto fut3 = pool.submit(add, 6, 7);
-    auto fut4 = pool.submit([]() {std::cout << "Hello World" << std::endl; });
+    auto fut4 = pool.submit([]() { std::cout << "Hello World" << std::endl; });
+    auto fut5 = pool.submit(triple, 4);
+    auto fut6 = pool.submit([&myNum](){ return myNum.makeInt(3.3); });
 
-    // std::this_thread::sleep_for(std::chrono::seconds(5));
 
-    std::cout << fut3.get() << std::endl;
+    std::cout << "Future 1: " << fut1.get() << std::endl
+              << "Future 2: " << fut2.get() << std::endl
+              << "Future 3: " << fut3.get() << std::endl
+              << "Future 5: " << fut5.get() << std::endl
+              << "Future 6: " << fut6.get() << std::endl
+              << std::endl;
     
     std::cout << std::endl << "Main ran successfully!" << std::endl;
     return 0;
--- threadpool.h
+++ threadpool.h
@@ -34,99 +34,81 @@ class AnyCallable {
 };
 
 // THREAD POOL CLASS
-
 class ThreadPool {
-    public:
-        ThreadPool();
-        ~ThreadPool();
-        template <typename Callable, typename... Args>
-        auto submit(Callable&& func, Args&&... args) -> std::future<std::result_of_t<Callable(Args...)>>;
-        void stopWorkers();
-        
-    private:
-        int num_threads = std::max(1u, std::thread::hardware_concurrency() / 2);
-        std::queue<AnyCallable> jobQueue;
-        std::vector<std::thread> workers;
-        std::mutex jobQueueMutex;
-        std::condition_variable cond;
-        std::atomic_bool stopFlag{false};
-        void deployWorker();
-};
-
-ThreadPool::ThreadPool() {
-  // Launch threads in parallel
-  for (int i = 0; i < num_threads; ++i) {
-      workers.emplace_back(&ThreadPool::deployWorker, std::ref(*this));
-  }
-}
-
-ThreadPool::~ThreadPool() {
-  // Wait for all threads to finish
-  for (auto& worker : workers) {
-      if (worker.joinable()) {
-          worker.join();
+  public:
+    ThreadPool() { // Launch threads in parallel
+      for (int i = 0; i < num_threads; ++i) {
+          workers.emplace_back(&ThreadPool::deployWorker, std::ref(*this));
       }
-  }
-}
+    }
 
-template <typename Callable, typename... Args>
-auto ThreadPool::submit(Callable&& func, Args&&... args) -> std::future<std::result_of_t<Callable(Args...)>> {
+    ~ThreadPool() { // Wait for all threads to finish
+      for (auto& worker : workers) {
+        if (worker.joinable()) worker.join();
+      }
+    }
 
-  typedef std::result_of_t<Callable(Args...)> retType;
+    template <typename Callable, typename... Args>
+    auto submit(Callable&& func, Args&&... args) -> std::future<std::result_of_t<Callable(Args...)>> {
+      typedef std::result_of_t<Callable(Args...)> retType;
 
-  // Bind arguments to function, and make the callable a packaged_task
-  std::packaged_task<retType()> task(
-    std::bind(func, std::forward<Args>(args)...)
-  );
+      // Bind arguments to function, and make the callable a packaged_task
+      std::packaged_task<retType()> task(std::bind(func, std::forward<Args>(args)...));
 
-  // Pull out future to return to caller
-  std::future<retType> result = task.get_future();
-   
-  // Add function to queue and wake up thread
-  jobQueue.push(std::move(task));
-  cond.notify_one();
-
-  return result;
-}
-
-// Starts worker thread which infintely loops watching the queue for jobs
-void ThreadPool::deployWorker() {
-  while(!stopFlag.load()) {
-    // Use unique_lock to lock the mutex
-    std::unique_lock<std::mutex> lock(jobQueueMutex);
-
-    // Will unlock mutex and sleep until notify_one() signal (function enters jobQueue), 
-    // then wake up, lock the mutex and continue on
-    cond.wait(lock, [this](){ return (!jobQueue.empty() || stopFlag.load()); });
-
-    if (!stopFlag.load()) {
-      // Pop front function in queue
-      auto callable = std::move(jobQueue.front());
-      jobQueue.pop();
-
-      // Unlock mutex, and wake up one thread
-      lock.unlock();
+      // Get future to return to caller
+      std::future<retType> result = task.get_future();
       
-      // Run the function
-      callable.call();
+      // Add function to queue and wake up thread
+      jobQueue.push(std::move(task));
+      cond.notify_one();
+
+      return result;
     }
-  }
-}
 
-// Stops workers by setting stopFlag = 1
-void ThreadPool::stopWorkers() { 
-  // Set flag to stop workers from looping
-  stopFlag.store(true);
+    void stopWorkers() {
+      stopFlag.store(true); // Set flag to stop workers from looping
 
-  // Wake up workers
-  cond.notify_all(); 
+      cond.notify_all(); // Wake up workers
 
-  // Wait for all threads to finish
-    for (auto& worker : workers) {
-        if (worker.joinable()) {
-            worker.join();
+      // Wait for all threads to finish
+      for (auto& worker : workers) {
+          if (worker.joinable()) {
+              worker.join();
+          }
+      }
+    }
+        
+  private:
+    int num_threads = std::max(1u, std::thread::hardware_concurrency() / 2);
+    std::queue<AnyCallable> jobQueue;
+    std::vector<std::thread> workers;
+    std::mutex jobQueueMutex;
+    std::condition_variable cond;
+    std::atomic_bool stopFlag{false};
+
+    // Starts worker thread which infintely loops watching the queue for jobs
+    void deployWorker() { 
+      while(!stopFlag.load()) {
+        // Use unique_lock to lock the mutex
+        std::unique_lock<std::mutex> lock(jobQueueMutex);
+
+        // Will unlock mutex and sleep until notify_one() signal (function enters jobQueue), 
+        // then wake up, lock the mutex and continue on
+        cond.wait(lock, [this](){ return (!jobQueue.empty() || stopFlag.load()); });
+
+        if (!stopFlag.load()) {
+          // Pop front function in queue
+          auto callable = std::move(jobQueue.front());
+          jobQueue.pop();
+
+          // Unlock mutex, and wake up one thread
+          lock.unlock();
+          
+          // Run the function
+          callable.call();
         }
+      }
     }
-}
+};
 
 #endif
